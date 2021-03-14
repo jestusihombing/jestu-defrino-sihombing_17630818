4220b493e483a6878ba279669032b40329244fae
jestusihombing <jestudefrinosihombing@gmail.com>
2021-03-14 15:54:18 +0800

    commit pertama dari project paraktikum Jestu Defrino sihombing 17630818

---
+++ profile.php
@@ -0,0 +1,52 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/jestu.jpg" alt="Jestu Defrino Sihombing" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK:</strong></td>
+                        <td align="left" width="50%"> 17630818</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Jestu Defrino Sihombing</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Medan, 15 Desember 1993</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0812 6038 7638</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">jestudefrinosihombing@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/jestusihombing" target="_blank" style="text-decoration:none;">
+                                <img src="img/facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="https://www.instagram.com/jestusihombing/" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/jestu" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/twitter.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/jestu" target="_blank">
+                                <img src="img/medsos/linkedin.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
